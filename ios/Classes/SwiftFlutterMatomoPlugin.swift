55930a89e43d2ab9828d598db0fb392e808ab523
Alex Kh <dartsyms@gmail.com>
2019-08-19 13:20:56 +0400

    Replace extension method for trackingCartUpdate with the private one, replace iflets and empty defaults with guards

--- ios/Classes/SwiftFlutterMatomoPlugin.swift
+++ ios/Classes/SwiftFlutterMatomoPlugin.swift
@@ -4,82 +4,72 @@ import MatomoTracker
 
 
 public class SwiftFlutterMatomoPlugin: NSObject, FlutterPlugin {
-
+    
     var matomoTracker: MatomoTracker? = nil
-
+    
     public static func register(with registrar: FlutterPluginRegistrar) {
         let channel = FlutterMethodChannel(name: "flutter_matomo", binaryMessenger: registrar.messenger())
         let instance = SwiftFlutterMatomoPlugin()
         registrar.addMethodCallDelegate(instance, channel: channel)
     }
-
+    
+    private func trackCartUpdate(itemsNumber: Int) {
+        let event = Event(tracker: self, action: [], itemsNumber: itemsNumber)
+        matomoTracker?.track(event)
+    }
+    
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-        if (call.method.elementsEqual("initializeTracker")) {
-            let arguments = call.arguments as? NSDictionary
-            let url = arguments?["url"] as? String
-            let siteId = arguments?["siteId"] as? Int
-//      if(matomoTracker == nil){
-            matomoTracker = MatomoTracker(siteId: String(siteId!), baseURL: URL(string: url ?? "")!)
+        if(call.method.elementsEqual("initializeTracker")){
+            guard let arguments = call.arguments as? NSDictionary,
+                let url = arguments?["url"] as? String,
+                let siteId = arguments?["siteId"] as? Int else { return }
+            matomoTracker = MatomoTracker(siteId: String(siteId), baseURL: URL(string: url)!)
             matomoTracker?.logger = DefaultLogger(minLevel: .verbose)
-//      }
             result("Matomo:: \(url) initialized successfully.")
         }
-        if (call.method.elementsEqual("trackEvent")) {
-            let arguments = call.arguments as? NSDictionary
-            let widgetName = arguments?["widgetName"] as? String
-            let eventName = arguments?["eventName"] as? String
-            let eventAction = arguments?["eventAction"] as? String
-            matomoTracker?.track(eventWithCategory: widgetName ?? "", action: eventAction ?? "", name: eventName ?? "", number: 3)
+        if(call.method.elementsEqual("trackEvent")){
+            guard let arguments = call.arguments as? NSDictionary,
+                let widgetName = arguments?["widgetName"] as? String,
+                let eventName =  arguments?["eventName"] as? String,
+                let eventAction =  arguments?["eventAction"] as? String else { return }
+            matomoTracker?.track(eventWithCategory: widgetName, action: eventAction, name: eventName, number: 3)
             matomoTracker?.dispatch()
             result("Matomo:: trackScreen event \(eventName) sent")
         }
-        if (call.method.elementsEqual("trackScreen")) {
-            let arguments = call.arguments as? NSDictionary
-            let widgetName = arguments?["widgetName"] as? String
-            matomoTracker?.track(view: [widgetName ?? ""])
+        if(call.method.elementsEqual("trackScreen")){
+            guard let arguments = call.arguments as? NSDictionary,
+                let widgetName = arguments?["widgetName"] as? String else { return }
+            matomoTracker?.track(view:[widgetName])
             matomoTracker?.dispatch()
             result("Matomo:: trackScreen screen \(widgetName) sent")
         }
-        if (call.method.elementsEqual("trackDownload")) {
+        if(call.method.elementsEqual("trackDownload")){
             result("Matomo:: trackDownload initialized successfully.")
         }
-        if (call.method.elementsEqual("trackGoals")) {
+        if(call.method.elementsEqual("trackGoals")){
             result("Matomo:: trackGoals initialized successfully.")
         }
-        if (call.method.elementsEqual("dispatchEvents")) {
+        if(call.method.elementsEqual("dispatchEvents")){
             matomoTracker?.dispatch()
             result("Matomo:: events dispatched")
         }
         if (call.method.elementsEqual("cartUpdate")) {
-            let arguments = call.arguments as? NSDictionary
-            let totalCount = arguments["totalCount"] as Int ?? 0
+            guard let arguments = call.arguments as? NSDictionary,
+                let totalCount = (arguments["items"] as? [OrderItem]).count
             matomoTracker?.trackCartUpdate(items: totalCount)
-            matomoTracker?.dispatch()
-            result("Matomo:: cartUpdate sent")
         }
         if (call.method.elementsEqual("trackOrder")) {
-            let arguments = call.arguments as? NSDictionary
-            let orderId = call.arguments?["goalId"] as String ?? "NotexistentID" 
-            let items = arguments["items"] as [OrderItem] ?? []
-            // let revenue = {
-            //     var totalPrice: Float = 0.0
-            //     for item in items {
-            //         totalPrice += item.price * item.quntity
-            //     }
-            //     return totalPrice
-            // }
-    
-            let revenue = arguments?["totalPrice"] as Float ?? 0.0
+            guard let arguments = call.arguments as? NSDictionary,
+                let orderId = arguments["id"] as? String,
+                let items = arguments["items"] as? [OrderItem] else { return }
+            let revenue = {
+                var totalPrice: Float = 0.0
+                for item in items {
+                    totalPrice += item.price * item.quntity
+                }
+                return totalPrice
+            }
             matomoTracker?.trackOrder(id: orderId, items: items, revenue: revenue)
-            matomoTracker?.dispatch()
-            result("Matomo:: trackOrder sent")
         }
-    }
-}
-
-extension MatomoTracker {
-    public func trackCartUpdate(itemsNumber: Int) {
-        let event = Event(tracker: self, action: [], itemsNumber: itemsNumber)
-        queue(event: event)
     }
 }
