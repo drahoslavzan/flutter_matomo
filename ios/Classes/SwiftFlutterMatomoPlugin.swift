67e50a30b41579309bc7a88b0f1f328f8dd4330b
Nick <nick@udev.dev>
2019-08-19 18:12:18 +0400

    clean code

--- ios/Classes/SwiftFlutterMatomoPlugin.swift
+++ ios/Classes/SwiftFlutterMatomoPlugin.swift
@@ -14,31 +14,42 @@ public class SwiftFlutterMatomoPlugin: NSObject, FlutterPlugin {
     }
     
     private func trackCartUpdate(itemsNumber: Int) {
-        let event = Event(tracker: self, action: [], itemsNumber: itemsNumber)
+        guard matomoTracker != nil else {return}
+        
+        var counter = itemsNumber
+        
+        var items = [OrderItem]()//TODO need by count
+        
+        repeat {
+            items.append(OrderItem(sku: ""))
+            counter -= 1
+        } while counter > 0
+        
+        let event = Event(tracker: matomoTracker!, action: [String](), orderItems: items)
         matomoTracker?.track(event)
     }
     
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         if(call.method.elementsEqual("initializeTracker")){
             guard let arguments = call.arguments as? NSDictionary,
-                let url = arguments?["url"] as? String,
-                let siteId = arguments?["siteId"] as? Int else { return }
+                let url = arguments["url"] as? String,
+                let siteId = arguments["siteId"] as? Int else { return }
             matomoTracker = MatomoTracker(siteId: String(siteId), baseURL: URL(string: url)!)
             matomoTracker?.logger = DefaultLogger(minLevel: .verbose)
             result("Matomo:: \(url) initialized successfully.")
         }
         if(call.method.elementsEqual("trackEvent")){
             guard let arguments = call.arguments as? NSDictionary,
-                let widgetName = arguments?["widgetName"] as? String,
-                let eventName =  arguments?["eventName"] as? String,
-                let eventAction =  arguments?["eventAction"] as? String else { return }
+                let widgetName = arguments["widgetName"] as? String,
+                let eventName =  arguments["eventName"] as? String,
+                let eventAction =  arguments["eventAction"] as? String else { return }
             matomoTracker?.track(eventWithCategory: widgetName, action: eventAction, name: eventName, number: 3)
             matomoTracker?.dispatch()
             result("Matomo:: trackScreen event \(eventName) sent")
         }
         if(call.method.elementsEqual("trackScreen")){
             guard let arguments = call.arguments as? NSDictionary,
-                let widgetName = arguments?["widgetName"] as? String else { return }
+                let widgetName = arguments["widgetName"] as? String else { return }
             matomoTracker?.track(view:[widgetName])
             matomoTracker?.dispatch()
             result("Matomo:: trackScreen screen \(widgetName) sent")
@@ -50,22 +61,21 @@ public class SwiftFlutterMatomoPlugin: NSObject, FlutterPlugin {
             result("Matomo:: trackGoals initialized successfully.")
         }
         if(call.method.elementsEqual("dispatchEvents")){
-            matomoTracker?.dispatch()
+           // matomoTracker?.dispatch()
             result("Matomo:: events dispatched")
         }
         if (call.method.elementsEqual("cartUpdate")) {
             guard let arguments = call.arguments as? NSDictionary,
-                let totalCount = arguments["totalCount"] as Int else { return }
-            matomoTracker?.trackCartUpdate(items: totalCount)
-            matomoTracker?.dispatch()
+                let totalCount = arguments["totalCount"] as? Int else { return }
+            trackCartUpdate(itemsNumber: totalCount)
             result("Matomo:: cartUpdate sent")
         }
         if (call.method.elementsEqual("trackOrder")) {
             guard let arguments = call.arguments as? NSDictionary,
-                let orderId = call.arguments?["goalId"] as String,
-                let items = arguments["items"] as [OrderItem] else { return }
+                let orderId = arguments["goalId"] as? String,
+                let items = arguments["items"] as? [OrderItem] else { return }
             
-            let revenue = arguments?["totalPrice"] as Float ?? 0.0
+            let revenue = arguments["totalPrice"] as! Float
             matomoTracker?.trackOrder(id: orderId, items: items, revenue: revenue)
             matomoTracker?.dispatch()
             result("Matomo:: trackOrder sent")
