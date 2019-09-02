a454373f05bacd5b98d2023fd719aabc57befa75
Nick Ivanov <>
2019-09-02 17:18:56 +0400

    added track download app

--- ios/Classes/SwiftFlutterMatomoPlugin.swift
+++ ios/Classes/SwiftFlutterMatomoPlugin.swift
@@ -13,22 +13,35 @@ public class SwiftFlutterMatomoPlugin: NSObject, FlutterPlugin {
         registrar.addMethodCallDelegate(instance, channel: channel)
     }
     
+    private func trackDownloadInternal(){
+        guard matomoTracker != nil else {return}
+        let urlString = "/application/downloaded?download=\(getInstallIdentifier())"
+
+        let event = Event(tracker: matomoTracker!, action: ["application/downloaded"], url: URL(string: urlString), referer: nil, eventCategory: "Application", eventAction: "downloaded" )
+        matomoTracker?.track(event)
+
+    }
+
+    private func getInstallIdentifier() -> String {
+        return "http://\(String(describing: Bundle.main.bundleIdentifier)):\(Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "")"
+    }
+
     private func trackCartUpdate(itemsNumber: Int) {
         guard matomoTracker != nil else {return}
-        
+
         var counter = itemsNumber
-        
+
         var items = [OrderItem]()
-        
+
         repeat {
             items.append(OrderItem(sku: ""))
             counter -= 1
         } while counter > 0
-        
+
         let event = Event(tracker: matomoTracker!, action: [String](), orderItems: items)
         matomoTracker?.track(event)
     }
-    
+
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         if(call.method.elementsEqual("initializeTracker")){
             guard let arguments = call.arguments as? NSDictionary,
@@ -36,6 +49,11 @@ public class SwiftFlutterMatomoPlugin: NSObject, FlutterPlugin {
                 let siteId = arguments["siteId"] as? Int else { return }
             matomoTracker = MatomoTracker(siteId: String(siteId), baseURL: URL(string: url)!)
             matomoTracker?.logger = DefaultLogger(minLevel: .verbose)
+            if (!UserDefaults.standard.bool(forKey: "isInstalled")){
+                trackDownloadInternal()
+                UserDefaults.standard.set(true, forKey: "isInstalled")
+            }
+
             result("Matomo:: \(url) initialized successfully.")
         }
         if(call.method.elementsEqual("trackEvent")){
